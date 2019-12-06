220f7da77590413d831252c05d83eb12d1ae80c0
raphaelgyo <raphael.granizo@gmail.com>
2019-12-06 09:41:16 +0100

    v2

--- index.php
+++ index.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <title>PHP Partie 4 Exercice 6 : les conditions</title>
   </head>
   <body>
-    <p>
-      <?php
-      for ($i = 1; $i <= 100; $i++) {
-        $output = '';
-        if ( $i%3 == 0) {
-          $output .= 'fizz';
-        }
-        if ( $i%5 == 0) {
-          $output .= 'buzz';
-        }
-        if ($output == '') {
-          $output .= $i;
-        }
-        echo $output. "\n";
+    <?php
+    for ($i = 1; $i <= 100; $i++) {
+      $output = '';
+      if ($i%3 == 0) {
+        $output = 'fizz';
       }
-      ?>
-    </p>
+      if ($i%5 == 0) {
+        // la concaténation pour bien indiquer le résultat fizzbuzz
+        $output .= 'buzz';
+      }
+      if ($output == '') {
+        $output = $i;
+      }
+    ?>
+    <p><?php echo $output; ?></p>
+    <?php } ?>
   </body>
 </html>
